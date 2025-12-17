078ff826e8e9f1def78bdb9753109506270434d0
cycy <96785236+lovecycy@users.noreply.github.com>
2025-12-17 23:16:28 +0800

    完成【乘客端登录-微信小程序登录接口】

--- service/service-customer/src/main/java/com/atguigu/daijia/customer/service/impl/CustomerInfoServiceImpl.java
+++ service/service-customer/src/main/java/com/atguigu/daijia/customer/service/impl/CustomerInfoServiceImpl.java
@@ -1,10 +1,17 @@
 package com.atguigu.daijia.customer.service.impl;
 
+import cn.binarywang.wx.miniapp.api.WxMaService;
+import cn.binarywang.wx.miniapp.bean.WxMaJscode2SessionResult;
 import com.atguigu.daijia.customer.mapper.CustomerInfoMapper;
+import com.atguigu.daijia.customer.mapper.CustomerLoginLogMapper;
 import com.atguigu.daijia.customer.service.CustomerInfoService;
 import com.atguigu.daijia.model.entity.customer.CustomerInfo;
+import com.atguigu.daijia.model.entity.customer.CustomerLoginLog;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import lombok.extern.slf4j.Slf4j;
+import me.chanjar.weixin.common.error.WxErrorException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,4 +20,42 @@ import org.springframework.stereotype.Service;
 @SuppressWarnings({"unchecked", "rawtypes"})
 public class CustomerInfoServiceImpl extends ServiceImpl<CustomerInfoMapper, CustomerInfo> implements CustomerInfoService {
 
+    @Autowired
+    private WxMaService wxMaService;
+
+    @Autowired
+    private CustomerInfoMapper customerInfoMapper;
+
+    @Autowired
+    private CustomerLoginLogMapper customerLoginLogMapper;
+
+    @Override
+    public Long login(String code) {
+        WxMaJscode2SessionResult sessionInfo = null;
+        try {
+            sessionInfo = wxMaService.getUserService().getSessionInfo(code);
+        } catch (WxErrorException e) {
+            throw new RuntimeException(e);
+        }
+        String openid = sessionInfo.getOpenid();
+
+        LambdaQueryWrapper<CustomerInfo> wrapper = new LambdaQueryWrapper<>();
+        wrapper.eq(CustomerInfo::getWxOpenId, openid);
+        CustomerInfo customerInfo = customerInfoMapper.selectOne(wrapper);
+
+        if (customerInfo == null) {
+            customerInfo = new CustomerInfo();
+            customerInfo.setWxOpenId(openid);
+            customerInfo.setNickname(String.valueOf(System.currentTimeMillis()));
+            customerInfo.setAvatarUrl("https://oss.aliyuncs.com/aliyun_id_photo_bucket/default_handsome.jpg");
+            customerInfoMapper.insert(customerInfo);
+        }
+
+        CustomerLoginLog customerLoginLog = new CustomerLoginLog();
+        customerLoginLog.setCustomerId(customerInfo.getId());
+        customerLoginLog.setMsg("小程序登录");
+        customerLoginLogMapper.insert(customerLoginLog);
+
+        return customerInfo.getId();
+    }
 }
--- web/web-customer/src/main/java/com/atguigu/daijia/customer/service/impl/CustomerServiceImpl.java
+++ web/web-customer/src/main/java/com/atguigu/daijia/customer/service/impl/CustomerServiceImpl.java
@@ -1,16 +1,52 @@
 package com.atguigu.daijia.customer.service.impl;
 
+import com.atguigu.daijia.common.constant.RedisConstant;
+import com.atguigu.daijia.common.execption.GuiguException;
+import com.atguigu.daijia.common.result.Result;
+import com.atguigu.daijia.common.result.ResultCodeEnum;
 import com.atguigu.daijia.customer.client.CustomerInfoFeignClient;
 import com.atguigu.daijia.customer.service.CustomerService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @Service
 @SuppressWarnings({"unchecked", "rawtypes"})
 public class CustomerServiceImpl implements CustomerService {
 
+    @Autowired
+    private CustomerInfoFeignClient client;
 
+    @Autowired
+    private RedisTemplate redisTemplate;
 
+    @Override
+    public String login(String code) {
+        Result<Long> loginResult = client.login(code);
+
+        Integer codeResult = loginResult.getCode();
+        if (codeResult != 200) {
+            throw new GuiguException(ResultCodeEnum.FEIGN_FAIL);
+        }
+
+        Long customerId = loginResult.getData();
+        if (customerId == null) {
+            throw new GuiguException(ResultCodeEnum.FEIGN_FAIL);
+        }
+
+        String token = UUID.randomUUID().toString().replace("-", "");
+        redisTemplate.opsForValue().set(
+                RedisConstant.USER_LOGIN_KEY_PREFIX + token,
+                customerId.toString(),
+                RedisConstant.USER_LOGIN_KEY_TIMEOUT,
+                TimeUnit.SECONDS
+        );
+
+        return token;
+    }
 }
