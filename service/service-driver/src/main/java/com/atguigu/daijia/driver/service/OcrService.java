d4747277e1d1ba35ee5728b6b793a5b28e95c1d8
cycy <96785236+lovecycy@users.noreply.github.com>
2026-01-16 00:47:28 +0800

    完成【司机端登录与认证-腾讯云身份证驾驶证识别接口】

--- service/service-driver/src/main/java/com/atguigu/daijia/driver/service/OcrService.java
+++ service/service-driver/src/main/java/com/atguigu/daijia/driver/service/OcrService.java
@@ -1,6 +1,14 @@
 package com.atguigu.daijia.driver.service;
 
+import com.atguigu.daijia.model.vo.driver.CosUploadVo;
+import com.atguigu.daijia.model.vo.driver.DriverLicenseOcrVo;
+import com.atguigu.daijia.model.vo.driver.IdCardOcrVo;
+import org.springframework.web.multipart.MultipartFile;
+
 public interface OcrService {
 
 
+    IdCardOcrVo idCardOcr(MultipartFile file);
+
+    DriverLicenseOcrVo driverLicenseOcr(MultipartFile file);
 }
--- service/service-driver/src/main/java/com/atguigu/daijia/driver/service/impl/OcrServiceImpl.java
+++ service/service-driver/src/main/java/com/atguigu/daijia/driver/service/impl/OcrServiceImpl.java
@@ -1,13 +1,136 @@
 package com.atguigu.daijia.driver.service.impl;
 
+import com.alibaba.nacos.common.codec.Base64;
+import com.atguigu.daijia.common.execption.GuiguException;
+import com.atguigu.daijia.common.result.ResultCodeEnum;
+import com.atguigu.daijia.driver.config.TencentCloudProperties;
+import com.atguigu.daijia.driver.service.CosService;
 import com.atguigu.daijia.driver.service.OcrService;
+import com.atguigu.daijia.model.vo.driver.CosUploadVo;
+import com.atguigu.daijia.model.vo.driver.DriverLicenseOcrVo;
+import com.atguigu.daijia.model.vo.driver.IdCardOcrVo;
+import com.tencentcloudapi.common.Credential;
+import com.tencentcloudapi.common.exception.TencentCloudSDKException;
+import com.tencentcloudapi.common.profile.ClientProfile;
+import com.tencentcloudapi.common.profile.HttpProfile;
+import com.tencentcloudapi.ocr.v20181119.OcrClient;
+import com.tencentcloudapi.ocr.v20181119.models.DriverLicenseOCRRequest;
+import com.tencentcloudapi.ocr.v20181119.models.DriverLicenseOCRResponse;
+import com.tencentcloudapi.ocr.v20181119.models.IDCardOCRRequest;
+import com.tencentcloudapi.ocr.v20181119.models.IDCardOCRResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.joda.time.format.DateTimeFormat;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @Slf4j
 @Service
 @SuppressWarnings({"unchecked", "rawtypes"})
 public class OcrServiceImpl implements OcrService {
 
+    @Autowired
+    private TencentCloudProperties tencentCloudProperties;
 
+    @Autowired
+    private CosService cosService;
+
+    @Override
+    public IdCardOcrVo idCardOcr(MultipartFile file) {
+        try {
+            IDCardOCRResponse resp = getIdCardOCRResponse(file);
+
+            IdCardOcrVo idCardOcrVo = new IdCardOcrVo();
+            if (StringUtils.hasText(resp.getName())){
+                idCardOcrVo.setName(resp.getName());
+                idCardOcrVo.setGender("男".equals(resp.getSex()) ? "1" : "2");
+                idCardOcrVo.setBirthday(DateTimeFormat.forPattern("yyyy/MM/dd").parseDateTime(resp.getBirth()).toDate());
+                idCardOcrVo.setIdcardNo(resp.getIdNum());
+                idCardOcrVo.setIdcardAddress(resp.getAddress());
+                CosUploadVo cosUploadVo = cosService.upload(file, "idCard");
+                idCardOcrVo.setIdcardFrontUrl(cosUploadVo.getUrl());
+                idCardOcrVo.setIdcardFrontShowUrl(cosUploadVo.getShowUrl());
+            } else {
+                String idcardExpireString = resp.getValidDate().split("-")[1];
+                idCardOcrVo.setIdcardExpire(DateTimeFormat.forPattern("yyyy.MM.dd").parseDateTime(idcardExpireString).toDate());
+                CosUploadVo cosUploadVo = cosService.upload(file, "idCard");
+                idCardOcrVo.setIdcardBackUrl(cosUploadVo.getUrl());
+                idCardOcrVo.setIdcardBackShowUrl(cosUploadVo.getShowUrl());
+            }
+
+
+            return idCardOcrVo;
+        } catch (Exception e) {
+            throw new GuiguException(ResultCodeEnum.DATA_ERROR);
+        }
+    }
+
+    @Override
+    public DriverLicenseOcrVo driverLicenseOcr(MultipartFile file) {
+        try {
+            DriverLicenseOCRResponse resp = getDriverLicenseOCRResponse(file);
+            DriverLicenseOcrVo driverLicenseOcrVo = new DriverLicenseOcrVo();
+            if (StringUtils.hasText(resp.getAddress())){
+                driverLicenseOcrVo.setName(resp.getName());
+                driverLicenseOcrVo.setDriverLicenseClazz(resp.getClass_());
+                driverLicenseOcrVo.setDriverLicenseNo(resp.getCardCode());
+                driverLicenseOcrVo.setDriverLicenseIssueDate(DateTimeFormat.forPattern("yyyy-MM-dd").parseDateTime(resp.getDateOfFirstIssue()).toDate());
+                driverLicenseOcrVo.setDriverLicenseExpire(DateTimeFormat.forPattern("yyyy-MM-dd").parseDateTime(resp.getEndDate()).toDate());
+                CosUploadVo cosUploadVo = cosService.upload(file, "driverLicense");
+                driverLicenseOcrVo.setDriverLicenseFrontUrl(cosUploadVo.getUrl());
+                driverLicenseOcrVo.setDriverLicenseFrontShowUrl(cosUploadVo.getShowUrl());
+            } else {
+                CosUploadVo cosUploadVo = cosService.upload(file, "driverLicense");
+                driverLicenseOcrVo.setDriverLicenseBackUrl(cosUploadVo.getUrl());
+                driverLicenseOcrVo.setDriverLicenseBackShowUrl(cosUploadVo.getShowUrl());
+            }
+
+            return driverLicenseOcrVo;
+        } catch (Exception e) {
+            throw new GuiguException(ResultCodeEnum.DATA_ERROR);
+        }
+
+    }
+
+    private IDCardOCRResponse getIdCardOCRResponse(MultipartFile file) throws TencentCloudSDKException, IOException {
+        byte[] base64 = Base64.encodeBase64(file.getBytes());
+        String fileBase64 = new String(base64);
+
+        Credential cred = new Credential(tencentCloudProperties.getSecretId(), tencentCloudProperties.getSecretKey());
+
+        HttpProfile httpProfile = new HttpProfile();
+        httpProfile.setEndpoint("ocr.tencentcloudapi.com");
+
+        ClientProfile clientProfile = new ClientProfile();
+        clientProfile.setHttpProfile(httpProfile);
+
+        IDCardOCRRequest req = new IDCardOCRRequest();
+        req.setImageBase64(fileBase64);
+
+        OcrClient client = new OcrClient(cred, tencentCloudProperties.getRegion(), clientProfile);
+        return client.IDCardOCR(req);
+    }
+
+    private DriverLicenseOCRResponse getDriverLicenseOCRResponse(MultipartFile file) throws TencentCloudSDKException, IOException {
+        byte[] base64 = Base64.encodeBase64(file.getBytes());
+        String fileBase64 = new String(base64);
+
+        Credential cred = new Credential(tencentCloudProperties.getSecretId(), tencentCloudProperties.getSecretKey());
+
+        HttpProfile httpProfile = new HttpProfile();
+        httpProfile.setEndpoint("ocr.tencentcloudapi.com");
+
+        ClientProfile clientProfile = new ClientProfile();
+        clientProfile.setHttpProfile(httpProfile);
+
+        DriverLicenseOCRRequest req = new DriverLicenseOCRRequest();
+        req.setImageBase64(fileBase64);
+        req.setCardSide("DOUBLE");
+
+        OcrClient client = new OcrClient(cred, tencentCloudProperties.getRegion(), clientProfile);
+        return client.DriverLicenseOCR(req);
+    }
 }
