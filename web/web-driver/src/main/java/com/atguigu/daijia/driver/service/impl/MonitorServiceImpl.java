40556a42b3a02f6aefd68e32ae4fd27aafa0e99f
cycy <96785236+lovecycy@users.noreply.github.com>
2026-01-31 03:41:08 +0800

    完成【订单执行-保存订单监控数据接口】

--- web/web-driver/src/main/java/com/atguigu/daijia/driver/service/impl/MonitorServiceImpl.java
+++ web/web-driver/src/main/java/com/atguigu/daijia/driver/service/impl/MonitorServiceImpl.java
@@ -1,13 +1,35 @@
 package com.atguigu.daijia.driver.service.impl;
 
+import com.atguigu.daijia.driver.service.FileService;
 import com.atguigu.daijia.driver.service.MonitorService;
+import com.atguigu.daijia.model.entity.order.OrderMonitorRecord;
+import com.atguigu.daijia.model.form.order.OrderMonitorForm;
+import com.atguigu.daijia.order.client.OrderMonitorFeignClient;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 @Slf4j
 @Service
 @SuppressWarnings({"unchecked", "rawtypes"})
 public class MonitorServiceImpl implements MonitorService {
 
+    @Autowired
+    private OrderMonitorFeignClient orderMonitorFeignClient;
 
+    @Autowired
+    private FileService fileService;
+
+    @Override
+    public Boolean upload(MultipartFile file, OrderMonitorForm orderMonitorForm) {
+        String url = fileService.upload(file);
+
+        OrderMonitorRecord orderMonitorRecord = new OrderMonitorRecord();
+        orderMonitorRecord.setFileUrl(url);
+        BeanUtils.copyProperties(orderMonitorForm, orderMonitorRecord);
+        orderMonitorFeignClient.saveMonitorRecord(orderMonitorRecord);
+        return true;
+    }
 }
