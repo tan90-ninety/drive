c2ab607fe372dc1f95aedb9b7bcd427ee1e6c9e3
cycy <96785236+lovecycy@users.noreply.github.com>
2026-02-02 02:54:54 +0800

    完成【订单执行-计算分账信息】

--- service/service-rules/src/main/java/com/atguigu/daijia/rules/service/impl/ProfitsharingRuleServiceImpl.java
+++ service/service-rules/src/main/java/com/atguigu/daijia/rules/service/impl/ProfitsharingRuleServiceImpl.java
@@ -1,10 +1,21 @@
 package com.atguigu.daijia.rules.service.impl;
 
+import com.atguigu.daijia.model.entity.rule.ProfitsharingRule;
+import com.atguigu.daijia.model.form.rules.ProfitsharingRuleRequest;
+import com.atguigu.daijia.model.form.rules.ProfitsharingRuleRequestForm;
+import com.atguigu.daijia.model.vo.rules.ProfitsharingRuleResponse;
+import com.atguigu.daijia.model.vo.rules.ProfitsharingRuleResponseVo;
 import com.atguigu.daijia.rules.mapper.ProfitsharingRuleMapper;
 import com.atguigu.daijia.rules.service.ProfitsharingRuleService;
+import com.atguigu.daijia.rules.utils.DroolsHelper;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import lombok.extern.slf4j.Slf4j;
+import org.kie.api.runtime.KieSession;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Slf4j
 @Service
@@ -14,5 +25,31 @@ public class ProfitsharingRuleServiceImpl implements ProfitsharingRuleService {
     @Autowired
     private ProfitsharingRuleMapper rewardRuleMapper;
 
+    private final static String RULES_CUSTOMER_RULES_DRL = "rules/ProfitsharingRule.drl";
 
+    @Override
+    public ProfitsharingRuleResponseVo calculateOrderProfitsharingFee(ProfitsharingRuleRequestForm profitsharingRuleRequestForm) {
+        //封装传入对象
+        ProfitsharingRuleRequest profitsharingRuleRequest = new ProfitsharingRuleRequest();
+        profitsharingRuleRequest.setOrderAmount(profitsharingRuleRequestForm.getOrderAmount());
+        profitsharingRuleRequest.setOrderNum(profitsharingRuleRequestForm.getOrderNum());
+
+        //获取最新订单费用规则
+        KieSession kieSession = DroolsHelper.loadForRule(RULES_CUSTOMER_RULES_DRL);
+
+        //封装返回对象
+        ProfitsharingRuleResponse profitsharingRuleResponse = new ProfitsharingRuleResponse();
+        kieSession.setGlobal("profitsharingRuleResponse", profitsharingRuleResponse);
+        // 设置订单对象
+        kieSession.insert(profitsharingRuleRequest);
+        // 触发规则
+        kieSession.fireAllRules();
+        // 中止会话
+        kieSession.dispose();
+
+        //封装返回对象
+        ProfitsharingRuleResponseVo profitsharingRuleResponseVo = new ProfitsharingRuleResponseVo();
+        BeanUtils.copyProperties(profitsharingRuleResponse, profitsharingRuleResponseVo);
+        return profitsharingRuleResponseVo;
+    }
 }
