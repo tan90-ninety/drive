4089729592512c25eebd77088d7e48f0227867f6
cycy <96785236+lovecycy@users.noreply.github.com>
2026-02-02 01:46:47 +0800

    完成【订单执行-获取订单数和记账分账信息】

--- service-client/service-rules-client/src/main/java/com/atguigu/daijia/rules/client/RewardRuleFeignClient.java
+++ service-client/service-rules-client/src/main/java/com/atguigu/daijia/rules/client/RewardRuleFeignClient.java
@@ -1,9 +1,15 @@
 package com.atguigu.daijia.rules.client;
 
+import com.atguigu.daijia.common.result.Result;
+import com.atguigu.daijia.model.form.rules.RewardRuleRequestForm;
+import com.atguigu.daijia.model.vo.rules.RewardRuleResponseVo;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(value = "service-rules")
 public interface RewardRuleFeignClient {
 
-
+    @PostMapping("/rules/reward/calculateOrderRewardFee")
+    Result<RewardRuleResponseVo> calculateOrderRewardFee(@RequestBody RewardRuleRequestForm rewardRuleRequestForm);
 }
--- service/service-rules/src/main/java/com/atguigu/daijia/rules/service/impl/RewardRuleServiceImpl.java
+++ service/service-rules/src/main/java/com/atguigu/daijia/rules/service/impl/RewardRuleServiceImpl.java
@@ -1,15 +1,46 @@
 package com.atguigu.daijia.rules.service.impl;
 
-import com.atguigu.daijia.rules.mapper.RewardRuleMapper;
+import com.atguigu.daijia.model.form.rules.RewardRuleRequest;
+import com.atguigu.daijia.model.form.rules.RewardRuleRequestForm;
+import com.atguigu.daijia.model.vo.rules.RewardRuleResponse;
+import com.atguigu.daijia.model.vo.rules.RewardRuleResponseVo;
 import com.atguigu.daijia.rules.service.RewardRuleService;
+import com.atguigu.daijia.rules.utils.DroolsHelper;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.kie.api.runtime.KieSession;
 import org.springframework.stereotype.Service;
+
+import java.text.DateFormat;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 @Slf4j
 @Service
 @SuppressWarnings({"unchecked", "rawtypes"})
 public class RewardRuleServiceImpl implements RewardRuleService {
 
+    private final static String RULES_CUSTOMER_RULES_DRL = "rules/RewardRule.drl";
 
+    @Override
+    public RewardRuleResponseVo calculateOrderRewardFee(RewardRuleRequestForm rewardRuleRequestForm) {
+        RewardRuleRequest rewardRuleRequest = new RewardRuleRequest();
+        rewardRuleRequest.setOrderNum(rewardRuleRequestForm.getOrderNum());
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        rewardRuleRequest.setStartTime(LocalTime.now().format(formatter));
+
+        KieSession kieSession = DroolsHelper.loadForRule(RULES_CUSTOMER_RULES_DRL);
+
+        RewardRuleResponse rewardRuleResponse = new RewardRuleResponse();
+        kieSession.setGlobal("rewardRuleResponse", rewardRuleResponse);
+
+        kieSession.insert(rewardRuleRequest);
+        kieSession.fireAllRules();
+
+        kieSession.dispose();
+
+        RewardRuleResponseVo rewardRuleResponseVo = new RewardRuleResponseVo();
+        rewardRuleResponseVo.setRewardAmount(rewardRuleResponse.getRewardAmount());
+        return rewardRuleResponseVo;
+    }
 }
