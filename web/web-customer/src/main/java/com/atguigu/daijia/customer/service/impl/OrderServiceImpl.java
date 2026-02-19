d8910b4322e8249e3bdddc72de4e72914c618a85
cycy <96785236+lovecycy@users.noreply.github.com>
2026-02-19 15:12:39 +0800

    完成【优惠券使用-乘客端支付方法改造】

--- web/web-customer/src/main/java/com/atguigu/daijia/customer/service/impl/OrderServiceImpl.java
+++ web/web-customer/src/main/java/com/atguigu/daijia/customer/service/impl/OrderServiceImpl.java
@@ -3,6 +3,7 @@ package com.atguigu.daijia.customer.service.impl;
 import com.atguigu.daijia.common.execption.GuiguException;
 import com.atguigu.daijia.common.result.Result;
 import com.atguigu.daijia.common.result.ResultCodeEnum;
+import com.atguigu.daijia.coupon.client.CouponFeignClient;
 import com.atguigu.daijia.customer.client.CustomerInfoFeignClient;
 import com.atguigu.daijia.customer.service.OrderService;
 import com.atguigu.daijia.dispatch.client.NewOrderFeignClient;
@@ -12,6 +13,7 @@ import com.atguigu.daijia.map.client.MapFeignClient;
 import com.atguigu.daijia.map.client.WxPayFeignClient;
 import com.atguigu.daijia.model.entity.order.OrderInfo;
 import com.atguigu.daijia.model.enums.OrderStatus;
+import com.atguigu.daijia.model.form.coupon.UseCouponForm;
 import com.atguigu.daijia.model.form.customer.ExpectOrderForm;
 import com.atguigu.daijia.model.form.customer.SubmitOrderForm;
 import com.atguigu.daijia.model.form.map.CalculateDrivingLineForm;
@@ -36,6 +38,7 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.Date;
 
 @Slf4j
@@ -66,6 +69,9 @@ public class OrderServiceImpl implements OrderService {
 
     @Autowired
     private WxPayFeignClient wxPayFeignClient;
+
+    @Autowired
+    private CouponFeignClient couponFeignClient;
 
     // 预估订单数据
     @Override
@@ -202,15 +208,40 @@ public class OrderServiceImpl implements OrderService {
         //3.获取司机微信openId
         String driverOpenId = driverInfoFeignClient.getDriverOpenId(orderPayVo.getDriverId()).getData();
 
-        //4.封装微信下单对象，微信支付只关注以下订单属性
+        //4.处理优惠券
+        BigDecimal couponAmount = null;
+        //支付时选择过一次优惠券，如果支付失败或未支付，下次支付时不能再次选择，只能使用第一次选中的优惠券（前端已控制，后端再次校验）
+        if (null == orderPayVo.getCouponAmount() && null != createWxPaymentForm.getCustomerCouponId() && createWxPaymentForm.getCustomerCouponId() != 0) {
+            UseCouponForm useCouponForm = new UseCouponForm();
+            useCouponForm.setOrderId(orderPayVo.getOrderId());
+            useCouponForm.setCustomerCouponId(createWxPaymentForm.getCustomerCouponId());
+            useCouponForm.setOrderAmount(orderPayVo.getPayAmount());
+            useCouponForm.setCustomerId(createWxPaymentForm.getCustomerId());
+            couponAmount = couponFeignClient.useCoupon(useCouponForm).getData();
+        }
+
+        //5.更新账单优惠券金额
+        //支付金额
+        BigDecimal payAmount = orderPayVo.getPayAmount();
+        if (null != couponAmount) {
+            Boolean isUpdate = orderInfoFeignClient.updateCouponAmount(orderPayVo.getOrderId(), couponAmount).getData();
+            if (!isUpdate) {
+                throw new GuiguException(ResultCodeEnum.DATA_ERROR);
+            }
+            //当前支付金额 = 支付金额 - 优惠券金额
+            payAmount = payAmount.subtract(couponAmount);
+        }
+
+        //6.封装微信下单对象，微信支付只关注以下订单属性
         PaymentInfoForm paymentInfoForm = new PaymentInfoForm();
         paymentInfoForm.setCustomerOpenId(customerOpenId);
         paymentInfoForm.setDriverOpenId(driverOpenId);
         paymentInfoForm.setOrderNo(orderPayVo.getOrderNo());
-        paymentInfoForm.setAmount(orderPayVo.getPayAmount());
+        paymentInfoForm.setAmount(payAmount);
         paymentInfoForm.setContent(orderPayVo.getContent());
         paymentInfoForm.setPayWay(1);
-        return wxPayFeignClient.createWxPayment(paymentInfoForm).getData();
+        WxPrepayVo wxPrepayVo = wxPayFeignClient.createWxPayment(paymentInfoForm).getData();
+        return wxPrepayVo;
     }
 
     @Override
